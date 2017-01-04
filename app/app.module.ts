4780f2f62c808b3ab31329f81d92406849a7908b
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-04 23:28:35 +0800

    feature: Added jumbotron component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 import { NavbarComponent } from './components/navbar/navbar.component'
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component'
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, NavbarComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule],
+  declarations: [AppComponent, NavbarComponent, JumbotronComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
